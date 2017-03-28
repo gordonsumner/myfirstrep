d52922cf48ff32e5f70916eba239aef7ca1fd5ca
gordonsumner <gordonsumner@libero.it>
2017-03-28 21:56:24 +0200

    Revert "https://github.com/gordonsumner/myfirstrep/"
    This reverts commit 56a9fa296da22c29d5a53983c03098495ad5123c.

--- SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using SportsStore.Domain.Entities;
-
-namespace SportsStore.Domain.Abstract {
-    public interface IProductRepository {
-
-        IEnumerable<Product> Products { get; }
-    }
-}
